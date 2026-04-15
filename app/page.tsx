14fb846e3948723470cf9e553c8d339e86e1a58b
jasjjds <jasjjds>
2026-04-16 06:07:00 +0700

    first commit

--- app/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from 'next/navigation';
-
-export default function Home() {
-  // Chuyển hướng người dùng từ trang chủ gốc (/) thẳng vào màn hình Dashboard
-  redirect('/home');
-}
